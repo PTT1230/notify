03cd1c1847df6abb28ed9d591e8ce5c9f5bfeaa8
PTT1230 <yujinyoutai@gmail.com>
2022-08-19 14:04:05 +0900

    LINEログイン機能ファイル修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,33 +1,28 @@
-# frozen_string_literal: true
-
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i[line]
-
-  has_many :contents
+         :recoverable, :rememberable, :validatable, :omniauthable,
+         omniauth_providers: %i[line]
 
   def social_profile(provider)
-    social_profiles.find { |sp| sp.provider == provider.to_s }
+    social_profiles.select { |sp| sp.provider == provider.to_s }.first
   end
 
   def set_values(omniauth)
-    return if provider.to_s != omniauth['provider'].to_s || uid != omniauth['uid']
+    return if provider.to_s != omniauth["provider"].to_s || uid != omniauth["uid"]
+    credentials = omniauth["credentials"]
+    info = omniauth["info"]
 
-    credentials = omniauth['credentials']
-    info = omniauth['info']
-
-    access_token = credentials['refresh_token']
-    access_secret = credentials['secret']
+    access_token = credentials["refresh_token"]
+    access_secret = credentials["secret"]
     credentials = credentials.to_json
-    name = info['name']
+    name = info["name"]
     # self.set_values_by_raw_info(omniauth['extra']['raw_info'])
   end
 
   def set_values_by_raw_info(raw_info)
     self.raw_info = raw_info.to_json
-    save!
+    self.save!
   end
 end
