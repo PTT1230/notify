3d0deadca488d24bfee3a854f9094c1b06285838
PTT1230 <yujinyoutai@gmail.com>
2022-08-24 10:54:16 +0900

    Update linebot_controller.rb

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -12,14 +12,14 @@ class LinebotController < ApplicationController
  
   def callback
     body = request.body.read
- 
+
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
       error 400 do 'Bad Request' end
     end
- 
+
     events = client.parse_events_from(body)
- 
+
     events.each { |event|
     case event
     when Line::Bot::Event::Message
@@ -40,8 +40,8 @@ class LinebotController < ApplicationController
           end
         end
       end
+    end
     }
-  end
     head :ok
   end
 end
