58be920704180a79bc44422824ca06d3660aa5ca
PTT1230 <108722053+PTT1230@users.noreply.github.com>
2022-08-26 09:13:45 +0900

    トップ画面文章追加

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,7 +3,18 @@
 
 window.onload = function() {
   const spinner = document.getElementById('loading');
- 
   // Add .loaded to .loading
   spinner.classList.add('loaded');
 }
+
+$('#animation').css('visibility','hidden');
+$(window).scroll(function(){
+ var windowHeight = $(window).height(),
+     topWindow = $(window).scrollTop();
+ $('#animation').each(function(){
+  var targetPosition = $(this).offset().top;
+  if(topWindow > targetPosition - windowHeight + 100){
+   $(this).addClass("fadeInDown");
+  }
+ });
+});
