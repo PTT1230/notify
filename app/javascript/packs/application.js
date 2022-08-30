6f5382e31356694dde09f2f6b122a064590711f5
PTT1230 <108722053+PTT1230@users.noreply.github.com>
2022-08-30 23:55:49 +0900

    マウスカーソル追尾設定

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ require('particles.js')
 require('../channels/top_animetion.js')
 require('../channels/index.js')
 require('../channels/consumer.js')
+require('../channels/cursor.js')
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
