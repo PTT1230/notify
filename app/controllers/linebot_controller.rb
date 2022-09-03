f902ac64b1a77c4f0196db49f6b42fa4763e95fd
PTT1230 <108722053+PTT1230@users.noreply.github.com>
2022-09-03 14:00:21 +0900

    Update linebot_controller.rb

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -21,7 +21,6 @@ class LinebotController < ApplicationController
     end
 
     events = client.parse_events_from(body)
-
     events.each { |event|
       case event
       when Line::Bot::Event::Message
@@ -32,23 +31,20 @@ class LinebotController < ApplicationController
             text: event.message['text']
           }
           client.reply_message(event['replyToken'], message)
-          when Line::Bot::Event::MessageType::Follow #友達登録イベント
-            userId = event['source']['userId'] 
-            User.find_or_create_by(uid: userId)
-            when Line::Bot::Event::MessageType::Unfollow#友達削除イベント
-              userId = event['source']['userId']  
-              user = User.find_by(uid: userId)
-              user.destroy if user.present?
-            end
-
-            message = {
-              type: 'text',
-              text: event.message['text']
-            }
-          end
+        when Line::Bot::Event::MessageType::Follow #友達登録イベント
+          userId = event['source']['userId'] 
+          User.find_or_create_by(uid: userId)
+        when Line::Bot::Event::MessageType::Unfollow#友達削除イベント
+          userId = event['source']['userId']  
+          user = User.find_by(uid: userId)
+          user.destroy if user.present?
+        end
+        message = {
+          type: 'text',
+          text: event.message['text']
+        }
       end
     }
-
     head :ok
   end
 end
