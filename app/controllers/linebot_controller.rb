def94c6e0542eeb5e179aacead337c537ce3ff83
PTT1230 <108722053+PTT1230@users.noreply.github.com>
2022-09-03 15:52:19 +0900

    インデント修正

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -21,28 +21,24 @@ class LinebotController < ApplicationController
     events = client.parse_events_from(body)
 
     events.each { |event|
-    case event
-    when Line::Bot::Event::Message
-      
-      case event.type
-      when Line::Bot::Event::MessageType::Text
-        message = {
-          type: 'text',
-          text: event.message['text']
-        }
-        client.reply_message(event['replyToken'], message)
-        when Line::Bot::Event::MessageType::Follow #友達登録イベント
-          userId = event['source']['userId'] 
-          User.find_or_create_by(uid: userId)
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+          when Line::Bot::Event::MessageType::Text
+            message = {
+              type: 'text',
+              text: event.message['text']
+            }
+          client.reply_message(event['replyToken'], message)
+          when Line::Bot::Event::MessageType::Follow #友達登録イベント
+            userId = event['source']['userId'] 
+            User.find_or_create_by(uid: userId)
           when Line::Bot::Event::MessageType::Unfollow#友達削除イベント
             userId = event['source']['userId']  
             user = User.find_by(uid: userId)
             user.destroy if user.present?
-
-          end
         end
       end
-    end
     }
 
     head :ok
